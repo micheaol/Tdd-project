de02a646a7ba1769c22d2c22059b1c2ff7777c30
micheaol <micheaol80@gmail.com>
2022-02-03 20:58:02 +0100

    Add test case for fizzbuzz method

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -7,4 +7,21 @@ describe Solver do
             expect(fact_instance).to eq(2)
         end
     end
+
+    describe "#fizzbuzz" do
+        it "takes integer, N and returns a string if N is divisible by 3, return fizz" do
+            fizz_instance = Solver.new.fizzbuzz(9)
+            expect(fizz_instance).to eq("fizz")
+        end
+
+        it "takes integer, N and returns a string if N is divisible by 5, return buzz" do
+            fizz_instance = Solver.new.fizzbuzz(9)
+            expect(fizz_instance).to eq("fizz")
+        end
+
+        it "takes integer, N and returns a string if N is divisible by 3 and 5, return fizzbuzz" do
+            fizz_instance = Solver.new.fizzbuzz(9)
+            expect(fizz_instance).to eq("fizz")
+        end
+    end
 end
