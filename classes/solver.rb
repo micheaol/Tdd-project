4f78f34ff4c91b9651cf77aae070f43053b3ab5d
micheaol <micheaol80@gmail.com>
2022-02-04 12:55:46 +0000

    Fix linter

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 class Solver
-    def factorial(num)
-        total = 1
-        (1..num).each do |el|
-            total *= el
-        end
-        total
+  def factorial(num)
+    total = 1
+    (1..num).each do |el|
+      total *= el
     end
+    total
+  end
 
-    def reverse(str)
-        str.reverse
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+
+    elsif (num % 3).zero?
+      'fizz'
+
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num
     end
-
-    def fizzbuzz (num)
-       if num % 3 == 0 && num % 5 == 0
-        "fizzbuzz"
-
-       elsif num % 3 == 0
-           "fizz"
-
-        elsif num % 5 == 0
-            "buzz"
-       else
-           num
-       end
-    end
+  end
 end
--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 require '../classes/solver'
 
 describe Solver do
-    describe "#factorial" do
-        it "takes an integer, and returns the factorial for the number" do
-            fact_instance = Solver.new.factorial(2)
-            expect(fact_instance).to eq(2)
-        end
+  describe '#factorial' do
+    it 'takes an integer, and returns the factorial for the number' do
+      fact_instance = Solver.new.factorial(2)
+      expect(fact_instance).to eq(2)
+    end
+  end
+
+  describe '#reverse' do
+    it 'Takes a string, and return the string reversed' do
+      reverse_instance = Solver.new.reverse('hello')
+      expect(reverse_instance).to eq('olleh')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'takes integer, N and returns a string if N is divisible by 3, return fizz' do
+      fizz_instance = Solver.new.fizzbuzz(3)
+      expect(fizz_instance).to eq('fizz')
     end
 
-    describe '#reverse' do
-        it "Takes a string, and return the string reversed" do
-            reverse_instance = Solver.new.reverse("hello")
-            expect(reverse_instance).to eq("olleh")
-        end
+    it 'takes integer, N and returns a string if N is divisible by 5, return buzz' do
+      buzz_instance = Solver.new.fizzbuzz(5)
+      expect(buzz_instance).to eq('buzz')
     end
 
-    describe "#fizzbuzz" do
-        it "takes integer, N and returns a string if N is divisible by 3, return fizz" do
-            fizz_instance = Solver.new.fizzbuzz(3)
-            expect(fizz_instance).to eq("fizz")
-        end
-
-        it "takes integer, N and returns a string if N is divisible by 5, return buzz" do
-            buzz_instance = Solver.new.fizzbuzz(5)
-            expect(buzz_instance).to eq("buzz")
-        end
-
-        it "takes integer, N and returns a string if N is divisible by 3 and 5, return fizzbuzz" do
-            fizz_instance = Solver.new.fizzbuzz(15)
-            expect(fizz_instance).to eq("fizzbuzz")
-        end
+    it 'takes integer, N and returns a string if N is divisible by 3 and 5, return fizzbuzz' do
+      fizz_instance = Solver.new.fizzbuzz(15)
+      expect(fizz_instance).to eq('fizzbuzz')
     end
+  end
 end
