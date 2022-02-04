150223fa6e1212b179ffc41fa6763e8a394ba880
Juan Carlos <jchernandez827@gmail.com>
2022-02-04 07:14:24 -0500

    Add test case for reverse method

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -8,6 +8,13 @@ describe Solver do
         end
     end
 
+    describe '#reverse' do
+        it "Takes a string, and return the string reversed" do
+            reverse_instance = Solver.new.reverse("hello")
+            exoect(reverse_instance).to eq("olleh")
+        end
+    end
+
     describe "#fizzbuzz" do
         it "takes integer, N and returns a string if N is divisible by 3, return fizz" do
             fizz_instance = Solver.new.fizzbuzz(3)
